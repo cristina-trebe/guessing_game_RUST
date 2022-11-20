31f301757286a31ce88299643d911aa99d8c700a
Cristina Trebe <cristinatrebe@yahoo.com>
2022-11-20 02:43:32 +0100

    game updated

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,38 @@ fn main() {
     //generate a random number to guess
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}"); // just for testing the random secret_number
 
-    println!("Please unput your guess.");
+    loop {
+        println!("Please input your guess:");
+    
+        let mut guess= String::new();
+    
+        //getting input from the keyboard 
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        //convert String as input into a real number
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        //and then printing the input
+        println!("You guessed: {guess}");
+    
+    
+        //Comparing "guess" to "secret_number"
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                                println!("You win!");
+                                break;
+                               }
+        }
 
-    let mut guess= String::new();
-
-    //getting input from the keyboard 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    //convert String as input into a real number
-    let guess:u32 = guess.trim().parse().expect("Please type a number!");
-
-    //and then printing the input
-    println!("You guessed: {guess}");
-
-    //Comparing the Guess to the Secret Number
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
     }
 
     
